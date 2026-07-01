dbe3dcdd498654abd5645c43017949f00b879110
Agaly Omarov <omaraly971215@gmail.com>
2026-07-01 18:57:56 +0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+   compatibilityDate: "2025-07-15",
+   devtools: { enabled: true },
+   ssr: false,
+   telemetry: false,
+   vite: {
+      // Better support for Tauri CLI output
+      clearScreen: false,
+      // Enable environment variables
+      // Additional environment variables can be found at
+      // https://v2.tauri.app/reference/environment-variables/
+      envPrefix: ["VITE_", "TAURI_"],
+      server: {
+         // Tauri requires a consistent port
+         strictPort: true,
+         watch: {
+            ignored: ["**/src-tauri/**"],
+         },
+      },
+      optimizeDeps: {
+         include: ["@vue/devtools-core", "@vue/devtools-kit"],
+      },
+   },
+   // Avoids error [unhandledRejection] EMFILE: too many open files, watch
+   ignore: ["**/src-tauri/**"],
+});
