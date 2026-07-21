a0c14f4ad6bed4aa53c701a8df24dcfea2c3f963
Agaly Omarov <omaraly971215@gmail.com>
2026-07-21 18:02:06 +0300

    yes

--- src-tauri/src/database/seeders/mod.rs
+++ src-tauri/src/database/seeders/mod.rs
@@ -1,8 +1,8 @@
 use anyhow::{Ok, Result};
 use sea_orm::DatabaseConnection;
-pub mod users;
+pub mod user_seed;
 
 pub async fn seeders(db: &DatabaseConnection) -> Result<()> {
-    users::users(db).await?;
+    user_seed::run(db).await?;
     Ok(())
 }
